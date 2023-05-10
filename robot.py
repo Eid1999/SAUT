cd14f54e546321d7fd781ac92efabb04b8d52f57
Eid1999 <pedrohenrique_eid@hotmail.com>
2023-05-10 13:24:34 +0100

    ROS_IMG to CV_Image

--- robot.py
+++ robot.py
@@ -1,10 +1,41 @@
+#!/usr/bin/env python
+import rospy
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from cv_bridge import CvBridge
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import os
+
+class image:
+    def __init__(self):
+        self.image = None
+        self.br = CvBridge()
+        # Node cycle rate (in Hz).
+        self.loop_rate = rospy.Rate(1)
+
+        # Publishers
+        self.pub = rospy.Publisher('imagetimer', Image,queue_size=10)
+
+        # Subscribers
+        rospy.Subscriber("/camera/image_color",Image,self.callback)
+
+
+    def callback(self, msg):
+        rospy.loginfo('Image received')
+        self.image = self.br.imgmsg_to_cv2(msg)
+
+
 
 
 class Robot:
-    def __init__(self, something):
+    def __init__(self):
+        sel.IMGS=image()
+        self.location=0
+        self.orientation=0
+    def listener(self):
         pass
 
     def odometry(self, v_linear, v_angular):
@@ -18,9 +49,13 @@ class Robot:
 
 
 def main():
-    something = 0
-    robot = Robot(something)
-    while True:
+    robot = Robot()
+
+    while not rospy.is_shutdown():
+        
+        if self.IMGS.image is not None:
+                self.IMGS.pub.publish(self.br.cv2_to_imgmsg(self.image))
+        self.IMGS.loop_rate.sleep()
         robot.kalman()
 
 
