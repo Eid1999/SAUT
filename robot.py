9c9c0a8889393b2b70e454659ca18d74d8565fd2
Eid1999 <pedrohenrique_eid@hotmail.com>
2023-05-10 09:50:06 +0100

    code template

---
+++ robot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import inv
+import cv2
+
+
+class Robot:
+    def __init__(self, something):
+        pass
+
+    def odometry(self, v_linear, v_angular):
+        pass
+
+    def kalman(self):
+        pass
+
+    def image_processing(self, map):
+        pass
+
+
+def main():
+    robot = Robot(something)
+    while (true):
+        robot.kalman()
+
+
+if __name__ == "__main__":
+    main()
