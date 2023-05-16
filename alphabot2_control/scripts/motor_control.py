0658491d1bd286ef607f8c7e44922cc768846d21
Eid1999 <pedrohenrique_eid@hotmail.com>
2023-05-16 21:58:30 +0100

    Pan_tilt control+fixes

--- alphabot2_control/scripts/motor_control.py
+++ alphabot2_control/scripts/motor_control.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
-
+from waveshare_alphabot2.msg import Pan_Tilt
+#fiducial_slam
 
 class control:
     def __init__(self):
         # Node cycle rate (in Hz).
         self.loop_rate = rospy.Rate(2)
         # Publishers
-        self.pub = rospy.Publisher('/cmd_vel', Twist,queue_size=10)
+        self.pub_motor = rospy.Publisher('/cmd_vel', Twist,queue_size=10)
+        self.pub_camera = rospy.Publisher('/pan_tilt', Pan_Tilt,queue_size=10)
 
     def message_creation(self,l_x=0,l_y=0,l_z=0,a_x=0,a_y=0,a_z=0):
         self.msg=Twist()
         self.msg.linear.x=l_x
         self.msg.linear.y=l_y
         self.msg.angular.z=a_z
+        
+        self.camera_msg = Pan_Tilt()
+        self.camera_msg.pan = 0.5
+        self.camera_msg.tilt = 0.5
+        
 def main():
     rospy.init_node("Motor_Control", anonymous=True)
     motor=control()
@@ -23,7 +30,8 @@ def main():
     while not rospy.is_shutdown():  
         motor.message_creation(l_x=0.2,l_y=.5,a_z=1.0)
         rospy.loginfo("Publishing Linear and angular velocities")        
-        motor.pub.publish(motor.msg)
+        motor.pub_motor.publish(motor.msg)
+        motor.pub_camera.publish(motor.camera_msg)
         motor.loop_rate.sleep()
 
 
